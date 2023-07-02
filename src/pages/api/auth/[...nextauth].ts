0768dd02b9a16448e1528fcacd2b204331ac6445
tfu2003 <officialtonyfu@gmail.com>
2023-07-02 14:22:47 -0700

    feat: add discord auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,33 +7,33 @@ import { env } from "../../../env.mjs";
 import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
+    // Include user.id on session
+    callbacks: {
+        session({ session, user }) {
+            if (session.user) {
+                session.user.id = user.id;
+            }
+            return session;
+        },
     },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID as string,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-      issuer: process.env.AUTH0_ISSUER
-    })
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Auth0Provider({
+            clientId: process.env.AUTH0_CLIENT_ID as string,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+            issuer: process.env.AUTH0_ISSUER
+        })
+        /**
+         * ...add more providers here
+         *
+         * Most other providers require a bit more work than the Discord provider.
+         * For example, the GitHub provider requires you to add the
+         * `refresh_token_expires_in` field to the Account model. Refer to the
+         * NextAuth.js docs for the provider you want to use. Example:
+         * @see https://next-auth.js.org/providers/github
+         */
+    ],
 };
 
 export default NextAuth(authOptions);

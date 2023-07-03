facf8b8374a8305a18feabbef1ea924e486a7813
Hanmin Kim <hanminkim123@gmail.com>
2023-07-02 21:32:31 -0700

    feat: add rest of the pages

---
+++ src/pages/sprint/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+
+
+  return (
+    <>
+      <Head>
+        <title>Calendar</title>
+        <meta name="description" content="Sprint" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="max-h-screen">
+          <div className="w-full max-h-screen overflow-y-hidden flex">
+
+          </div>
+      </main>
+    </>
+  );
+}

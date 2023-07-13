eab1fb4e5d214ddc57e37251797c16aaf6e88c4b
Hanmin Kim <hanminkim123@gmail.com>
2023-07-12 23:51:58 -0700

    feat: merge day and week views

---
+++ src/components/timeline/timeline-view.stories.tsx
@@ -0,0 +1,202 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {TimelineView} from './timeline-view';
+import { IActivity } from '~/utils/types';
+const meta: Meta<typeof TimelineView> = {
+    title: 'Day View',
+    component: TimelineView,
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TimelineView>;
+
+const activityOne: IActivity ={
+    id: "1",
+    name: "Task One",
+    at: new Date(2023, 7, 9, 12, 0, 0),
+    length: 2 * 60 * 1000
+}
+
+const activityTwo: IActivity ={
+    id: "1",
+    name: "Task two",
+    at: new Date(2023, 7, 9, 9, 0, 0),
+    length: 1 * 60 * 1000
+}
+
+const render = (args: any) => <div className="h-96">
+    <TimelineView {...args}/>
+</div>
+
+export const NoDayTasks: Story = {
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "2023-08-09",
+                activityList: []
+            }
+        ]
+    },
+    render
+};
+
+export const OneDayTask: Story ={
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "2023-08-09",
+                activityList: [ activityOne ]
+            }
+        ]
+    },
+    render
+}
+
+export const TwoDayTask: Story ={
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "2023-08-09",
+                activityList: [
+                    activityOne,
+                    activityTwo
+                ]
+            }
+        ]
+    },
+    render
+}
+
+export const NoWeekTasks: Story = {
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "Sunday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Monday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Tuesday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Wednesday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Thursday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Friday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Saturday",
+                activityList: [
+                ]
+            }
+        ]
+    },
+    render
+};
+
+export const OneWeekTask: Story ={
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "Sunday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Monday",
+                activityList: [
+                    activityOne
+                ]
+            },
+            {
+                dayLabel: "Tuesday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Wednesday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Thursday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Friday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Saturday",
+                activityList: [
+                ]
+            }
+        ]
+    },
+    render
+}
+
+export const TwoWeekTask: Story ={
+    args: {
+        dayViewList: [
+            {
+                dayLabel: "Sunday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Monday",
+                activityList: [
+                    activityOne
+                ]
+            },
+            {
+                dayLabel: "Tuesday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Wednesday",
+                activityList: [
+                    activityTwo
+                ]
+            },
+            {
+                dayLabel: "Thursday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Friday",
+                activityList: [
+                ]
+            },
+            {
+                dayLabel: "Saturday",
+                activityList: [
+                ]
+            }
+        ]
+    },
+    render
+}
+
--- src/pages/track/index.tsx
+++ src/pages/track/index.tsx
@@ -11,8 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="max-h-screen">
-          <div className="w-full max-h-screen overflow-y-hidden flex">
+          <div className="w-full max-h-screen flex">
+              <div>
 
+              </div>
           </div>
       </main>
     </>

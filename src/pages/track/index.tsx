67beae2f15356266b38988224d538ba1b2ae3ce0
Hanmin Kim <hanminkim123@gmail.com>
2023-07-13 00:08:13 -0700

    feat: add planned vs reality onto track

--- src/pages/track/index.tsx
+++ src/pages/track/index.tsx
@@ -1,22 +1,30 @@
 import Head from "next/head";
+import {TimelineView} from "~/components/timeline/timeline-view";
 
 export default function Home() {
 
 
-  return (
-    <>
-      <Head>
-        <title>Calendar</title>
-        <meta name="description" content="Track" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="max-h-screen">
-          <div className="w-full max-h-screen flex">
-              <div>
-
-              </div>
-          </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Calendar</title>
+                <meta name="description" content="Track"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <main className="max-h-screen">
+                <div className="w-full h-screen flex">
+                    <TimelineView dayViewList={[
+                        {
+                            dayLabel: 'Planned',
+                            activityList: []
+                        },
+                        {
+                            dayLabel: 'Reality',
+                            activityList: []
+                        }
+                    ]}/>
+                </div>
+            </main>
+        </>
+    );
 }

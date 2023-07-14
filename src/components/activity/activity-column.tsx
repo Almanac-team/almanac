f59c0ff948eff82f7cb8edcae276527b4197502a
Hanmin Kim <hanminkim123@gmail.com>
2023-07-14 02:32:12 -0700

    feat: make activityColumn text color based on background color

--- src/components/activity/activity-column.tsx
+++ src/components/activity/activity-column.tsx
@@ -13,15 +13,28 @@ export function ActivityColumn({categoryInfo, activities}: {
     categoryInfo: CategoryInfo,
     activities: ActivitySetting[]
 }) {
+    const hexToGray = (hex: string): number => {
+        const bigint = parseInt(hex.replace("#", ""), 16);
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+
+        const a = (r + g + b) / 255 / 3;
+        console.log(a);
+        return a;
+    }
+
+    const textColor = hexToGray(categoryInfo.backgroundColor) > 0.7 ? "text-gray-800" : "text-white";
+
     return (
         <div className="flex flex-col w-96 min-w-[20em] h-full">
             <div
                 className="flex flex-row items-center p-2 w-full justify-center" style={{backgroundColor: categoryInfo.backgroundColor}}>
-                <span className={clsx("font-bold", categoryInfo.textColor)}>{categoryInfo.categoryName}</span>
+                <span className={clsx("font-bold", textColor)}>{categoryInfo.categoryName}</span>
             </div>
             <div className="flex flex-col w-full flex-grow overflow-y-scroll space-y-2 py-2">
-                {activities.map((activity, index) => (
-                    <ActivityOverview key={activity.id} taskName={activity.name} activitySetting={activity} categoryInfo={categoryInfo} />
+                {activities.map((activity) => (
+                    <ActivityOverview key={activity.id} taskName={activity.name} activitySetting={activity} categoryInfo={{...categoryInfo, textColor}} />
                 ))}
             </div>
         </div>

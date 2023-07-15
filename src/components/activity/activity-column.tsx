22c0582ec5af3e7d0c54cdd10ca1f2f4ae66b18c
Hanmin Kim <hanminkim123@gmail.com>
2023-07-15 12:04:15 -0700

    feat: merge AddDefinition and AddActivityModal, and use union setting for ActivitySetting

--- src/components/activity/activity-column.tsx
+++ src/components/activity/activity-column.tsx
@@ -3,7 +3,14 @@ import {ActivityOverview} from "~/components/activity/activity-overview";
 import {api} from "~/utils/api";
 import {useState} from "react";
 import {Button, Menu, MenuHandler, MenuList} from "@material-tailwind/react";
-import {ActivitySetting, ActivityType, EventSetting, TaskSetting, TaskSettingConfig, EventSettingConfig} from "~/components/activity/activity-settings";
+import {
+    ActivitySetting,
+    ActivityType,
+    EventSetting,
+    TaskSetting,
+    TaskSettingConfig,
+    EventSettingConfig
+} from "~/components/activity/activity-settings";
 import {Tab, Tabs} from "~/components/activity/tab";
 
 export interface CategoryInfo {
@@ -13,35 +20,47 @@ export interface CategoryInfo {
     textColor: string;
 }
 
-function AddActivityModal(props: {
-    activitySetting: ActivitySetting,
-    onChange?: (activitySetting: ActivitySetting) => void,
-    onSubmit?: () => void
+function AddActivityModal({onSubmit}: {
+    onSubmit?: (activity: any) => void,
 }) {
+    const [activitySetting, setActivitySetting] = useState<ActivitySetting>({
+        id: "-1",
+        name: "",
+        activityType: 'task',
+        setting: undefined
+    });
 
-    function changeTaskSetting(taskSetting: TaskSetting) {
-        if (props.onChange) props.onChange({...props.activitySetting, taskSetting})
-    }
+    const [taskSetting, setTaskSetting] = useState<TaskSetting>(
+        {
+            due: new Date(),
+            estimatedRequiredTime: {value: 1, unit: "hour"},
+            deadlineMod: {value: 0, unit: "minute"},
+            reminderMod: {value: 0, unit: "minute"},
+            startMod: {value: 0, unit: "minute"},
+        }
+    );
 
-    function changeEventSetting(eventSetting: EventSetting) {
-        if (props.onChange) props.onChange({...props.activitySetting, eventSetting})
-    }
+    const [eventSetting, setEventSetting] = useState<EventSetting>(
+        {
+            at: new Date(),
+            estimatedRequiredTime: {value: 1, unit: "hour"},
+            reminderMod: {value: 0, unit: "minute"},
+            startMod: {value: 0, unit: "minute"},
+        }
+    );
 
     return (
         <div className="flex flex-col space-y-2 w-96 h-96 justify-start">
             <input
                 type="text"
-                value={props.activitySetting.name}
+                value={activitySetting.name}
                 className="p-2 mr-2 border-gray-300 border-b-2 focus:border-blue-500 focus:outline-none transition-colors text-gray-700 text-xl"
                 placeholder="Activity Name"
-                onChange={(e) => {
-                    if (props.onChange) props.onChange({...props.activitySetting, name: e.target.value})
-                }}
+                onChange={(e) => setActivitySetting({...activitySetting, name: e.target.value})}
             />
 
-            <Tabs activeValue={props.activitySetting.activityType} onChange={(type) => {
-                if (props.onChange) props.onChange({...props.activitySetting, activityType: type as ActivityType})
-            }}>
+            <Tabs activeValue={activitySetting.activityType}
+                  onChange={(type) => setActivitySetting({...activitySetting, activityType: type as ActivityType})}>
                 <Tab value="task">
                     Task
                 </Tab>
@@ -50,39 +69,12 @@ function AddActivityModal(props: {
                 </Tab>
             </Tabs>
 
-            {props.activitySetting.activityType === 'task' ?
-                <TaskSettingConfig taskSetting={props.activitySetting.taskSetting} onChange={changeTaskSetting}/> :
-                <EventSettingConfig eventSetting={props.activitySetting.eventSetting} onChange={changeEventSetting}/>}
+            {activitySetting.activityType === 'task' ?
+                <TaskSettingConfig taskSetting={taskSetting} onChange={setTaskSetting}/> :
+                <EventSettingConfig eventSetting={eventSetting} onChange={setEventSetting}/>}
             <Button>Save</Button>
         </div>
     );
-}
-
-function AddDefinition({onSubmit}: {
-    onSubmit?: (activity: any) => void,
-}) {
-    const [activitySetting, setActivitySetting] = useState<ActivitySetting>({
-        id: "-1",
-        name: "",
-        activityType: 'task',
-        taskSetting: {
-            due: new Date(),
-            estimatedRequiredTime: {value: 1, unit: "hour"},
-            deadlineMod: {value: 0, unit: "minute"},
-            reminderMod: {value: 0, unit: "minute"},
-            startMod: {value: 0, unit: "minute"},
-        },
-        eventSetting: {
-            at: new Date(),
-            estimatedRequiredTime: {value: 1, unit: "hour"},
-            reminderMod: {value: 0, unit: "minute"},
-            startMod: {value: 0, unit: "minute"},
-        }
-    });
-
-    return <AddActivityModal activitySetting={activitySetting}
-                             onChange={setActivitySetting}
-    />
 }
 
 const hexToGray = (hex: string): number => {
@@ -136,7 +128,7 @@ export function ActivityColumn({categoryInfo}: {
                     </div>
                 </MenuHandler>
                 <MenuList>
-                    <AddDefinition/>
+                    <AddActivityModal/>
                 </MenuList>
             </Menu>
         </div>

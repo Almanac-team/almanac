1262ba9b18ccee8210fb809735f42573ab9dc045
Hanmin Kim <hanminkim123@gmail.com>
2023-07-14 21:26:47 -0700

    style: lint activity-column

--- src/components/activity/activity-column.tsx
+++ src/components/activity/activity-column.tsx
@@ -29,17 +29,16 @@ export function ActivityColumn({categoryInfo, activities}: {
     return (
         <div className="flex flex-col w-96 min-w-[20em] h-full">
             <div
-                className="flex flex-row items-center p-2 w-full justify-center" style={{backgroundColor: categoryInfo.backgroundColor}}>
+                className="flex flex-row items-center p-2 w-full justify-center"
+                style={{backgroundColor: categoryInfo.backgroundColor}}>
                 <span className={clsx("font-bold", textColor)}>{categoryInfo.categoryName}</span>
             </div>
             <div className="flex flex-col w-full flex-grow overflow-y-scroll space-y-2 py-2">
                 {activities.map((activity) => (
-                    <ActivityOverview key={activity.id} taskName={activity.name} activitySetting={activity} categoryInfo={{...categoryInfo, textColor}} />
+                    <ActivityOverview key={activity.id} taskName={activity.name} activitySetting={activity}
+                                      categoryInfo={{...categoryInfo, textColor}}/>
                 ))}
             </div>
         </div>
-
-
     )
-
 }

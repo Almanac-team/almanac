1d47c5d6ea2a888118de60f3b96bd2c7c10491ce
Hanmin Kim <hanminkim123@gmail.com>
2023-07-15 16:01:11 -0700

    feat: add more activity procedures

--- src/server/api/routers/activities.ts
+++ src/server/api/routers/activities.ts
@@ -16,6 +16,89 @@ const activitiesRouter = createTRPCRouter({
         })
     }),
 
+    getDetailedActivities: protectedProcedure.input(z.object({
+        categoryId: z.string()
+    })).query(({ctx, input}) => {
+        const userId = ctx?.session?.user?.id
+        return ctx.prisma.activity.findMany({
+            where: {
+                category: {
+                    id: input.categoryId,
+                    userId: userId
+                }
+            },
+            include: {
+                task: true,
+                event: true
+            }
+        })
+    }),
+
+    getActivitySpecifics: protectedProcedure.input(z.object({
+        activityIds: z.array(z.string())
+    })).query(async ({ctx, input}) => {
+        const userId = ctx?.session?.user?.id
+        const ids = input.activityIds;
+        const reverseMap = ids.map((id, index) => {
+            return {id, index}
+        }).sort((a, b) => {
+            if (a.id < b.id) return -1
+            if (a.id > b.id) return 1
+            return 0
+        })
+
+        const tasks = await ctx.prisma.task.findMany({
+            where: {
+                activityId: {
+                    in: ids
+                },
+                activity: {
+                    category: {
+                        userId: userId
+                    }
+                }
+            },
+            orderBy: {
+                activityId: 'asc'
+            }
+        })
+        const events = await ctx.prisma.event.findMany({
+            where: {
+                activityId: {
+                    in: ids
+                },
+                activity: {
+                    category: {
+                        userId: userId
+                    }
+                }
+            },
+            orderBy: {
+                activityId: 'asc'
+            }
+        })
+
+        const result = []
+        let tp = 0
+        let ep = 0
+
+        for (const item of reverseMap) {
+            if (item.id === tasks[tp]?.activityId) {
+                result.push(tasks[tp])
+                tp++
+            } else if (item.id === events[ep]?.activityId) {
+                result.push(events[ep])
+                ep++
+            } else {
+                result.push(null)
+            }
+        }
+
+        if (tp !== tasks.length || ep !== events.length) return null
+
+        return result
+    }),
+
     getTask: protectedProcedure.input(z.object({
         id: z.string()
     })).query(({ctx, input}) => {

b0b294e61e56a12cfeb8eab932066f4ab6139ff4
Hanmin Kim <hanminkim123@gmail.com>
2023-07-22 14:50:45 -0700

    feat: integrate zone view

--- src/pages/zones/index.tsx
+++ src/pages/zones/index.tsx
@@ -5,6 +5,7 @@ import {useEffect, useState} from "react";
 import {Button} from "@material-tailwind/react";
 import {api} from "~/utils/api";
 import {ZoneColumn} from "~/components/zone/zone-column";
+import {ZoneView} from "~/components/zone/zone-view";
 
 export function getWeekStart(date: Date) {
     const today = date;
@@ -22,6 +23,15 @@ export interface GeneratedEvent {
 }
 
 export default function Home() {
+    const zones = api.zones.getZones.useQuery().data ?? [];
+    const [zoneIndex, setZoneIndex] = useState<number>(0);
+
+    useEffect(() => {
+        if (zones.length > zoneIndex) {
+            setZoneIndex(zones.length - 1);
+        }
+    }, [zones]);
+
     return (
         <>
             <Head>
@@ -30,9 +40,10 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className="max-h-screen h-screen flex flex-col">
-                <div className="flex flex-col">
+                <div className="h-screen flex flex-row">
+                    <ZoneColumn zones={zones} onSelect={(index: number) => setZoneIndex(index)}/>
+                    <ZoneView zone={zones[zoneIndex]}/>
                 </div>
-                <ZoneColumn/>
             </main>
         </>
     );

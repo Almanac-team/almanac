c79e6e9ada268290a54f7bca1dee935b6b2c76fa
Hanmin Kim <hanminkim123@gmail.com>
2023-07-22 19:45:19 -0700

    feat: add TimeLineInteractionContext

--- src/components/timeline/models.ts
+++ src/components/timeline/models.ts
@@ -1,4 +1,23 @@
-export interface ScheduledEvent {
+import {createContext} from "react";
+
+export const TimelineInteractionContext = createContext<TimelineInteraction>({});
+
+export interface TimelineInteraction {
+    selectedEventId?: string,
+    onBlockClick?: (eventId: string) => void,
+
+    onBlockDragStart?: (eventId: string) => void,
+    onBlockDrag?: (eventId: string, dragPercentDelta: number) => void,
+    onBlockDragEnd?: (eventId: string, dragPercentDelta: number) => void,
+
+    onBackgroundClick?: (verticalPercent: number) => void,
+
+    onBackgroundDragStart?: (verticalPercent: number, dragPercentDelta: number) => void,
+    onBackgroundDrag?: (verticalPercent: number, dragPercentDelta: number) => void,
+    onBackgroundDragEnd?: (verticalPercent: number, dragPercentDelta: number) => void,
+}
+
+export interface ScheduledBlock {
     id: string,
     name: string,
     date: Date,
@@ -8,5 +27,5 @@ export interface ScheduledEvent {
 export interface DayViewProps {
     startDay: Date,
     dayLabel: string,
-    activityList: ScheduledEvent[]
+    activityList: ScheduledBlock[]
 }

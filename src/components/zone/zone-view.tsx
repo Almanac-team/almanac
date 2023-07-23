a7c2f22a7fefc87c7094fe4b99880cc6ab49f390
Hanmin Kim <hanminkim123@gmail.com>
2023-07-22 18:44:23 -0700

    feat: create zone view

--- src/components/zone/zone-view.tsx
+++ src/components/zone/zone-view.tsx
@@ -1,24 +1,22 @@
-import {ZoneInfo} from "~/components/zone/zone-column";
-import {api} from "~/utils/api";
+import {type ZoneInfo} from "~/components/zone/zone-column";
+import {WeekView} from "~/components/timeline/timeline-view";
+import {type ScheduledEvent} from "~/utils/types";
+import {useState} from "react";
+import clsx from "clsx";
 
-export function ZoneView({zone}: { zone?: ZoneInfo }) {
+export function ZoneView({zone, className}: { zone?: ZoneInfo, className?: string }) {
+    const [activities, setActivities] = useState<ScheduledEvent[]>([]);
+    const firstDayMidnight = new Date(0, 0, 0, 0, 0, 0, 0);
+
     if (!zone) {
         return <></>;
     }
 
-
-    return (
-        <div className="flex flex-col">
-            <div className="flex flex-row items-center space-x-2">
-                <div className="w-4 h-4 rounded-full" style={{backgroundColor: zone.color}}/>
-                <div className="text-xl font-bold">{zone.name}</div>
-            </div>
-            <div className="flex flex-col space-y-2">
-                <div className="flex flex-row items-center space-x-2">
-                    <div className="w-4 h-4 rounded-full bg-gray-400"/>
-                    <div className="text-xl font-bold">Monday</div>
-                </div>
-            </div>
+    return <div className={clsx("flex flex-col max-h-full py-2", className)}>
+        <div className="flex flex-row items-center space-x-2">
+            <div className="w-4 h-4 rounded-full" style={{backgroundColor: zone.color}}/>
+            <div className="text-xl font-bold">{zone.name}</div>
         </div>
-    );
+        <WeekView className="w-full h-full min-h-0 border-b-2" activityList={activities} firstDayMidnight={firstDayMidnight}/>
+    </div>
 }

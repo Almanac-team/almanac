a2ef6ec8ade42cc3f114d9b35880022371c8da21
Hanmin Kim <hanminkim123@gmail.com>
2023-07-23 01:22:34 -0700

    feat: add region updating to zone view

--- src/components/zone/zone-view.tsx
+++ src/components/zone/zone-view.tsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import clsx from "clsx";
 import {type Region, type ZoneInfo} from "~/components/zone/models";
 import {TimelineInteractionContext} from "~/components/timeline/models";
 import {WeekView} from "~/components/timeline/timeline-view";
+import {api} from "~/utils/api";
 
 interface NotCreatedRegion {
     id: undefined,
@@ -10,22 +11,109 @@ interface NotCreatedRegion {
     to: number
 }
 
-export function ZoneView({zone, className}: { zone?: ZoneInfo, className?: string }) {
-    const [scheduledBlocks, setScheduledBlocks] = useState<(Region | NotCreatedRegion)[]>((zone?.regions ?? []));
+type DisplayRegionUnion = Region | NotCreatedRegion;
+
+export function ZoneView({zone, className}: { zone: ZoneInfo, className?: string }) {
+    const [scheduledBlocks, setScheduledBlocks] = useState<DisplayRegionUnion[]>((zone.regions));
+    const [changeList, setChangeList] = useState<string[]>([]);
+
+    const { mutate: createRegions } = api.regions.createRegions.useMutation();
+    const { mutate: updateRegions } = api.regions.updateRegions.useMutation();
+    const { mutate: deleteRegions } = api.regions.deleteRegions.useMutation();
+
 
     const firstDayMidnight = new Date(0);
+
+    const checkRedundancy = (scheduledBlocks: DisplayRegionUnion[]) => {
+        const newScheduledBlocks = [];
+
+        // this is horrible, but we'll replace the whole zone system soon
+        for (const region of scheduledBlocks) {
+            let shouldAdd = true;
+            for (const existingRegion of newScheduledBlocks) {
+                // if region overlaps existing region ends, extend the other region, and remove this one
+
+                if (!(existingRegion.to < region.from - 0.01 || region.to + 0.01 < existingRegion.from)) {
+                    existingRegion.to = Math.max(existingRegion.to, region.to);
+                    existingRegion.from = Math.min(existingRegion.from, region.from);
+                    shouldAdd = false;
+
+                    if (region.id !== undefined) {
+                        // if the region has an id, add it to the change list
+                        setChangeList(prev => [...prev, region.id])
+                    }
+
+                    if (!existingRegion.id) {
+                        // steal the id if it exists but does not for the existing region
+                        existingRegion.id = region.id;
+                    } else {
+                        // if the existingRegion had an id, add it to the change list
+                        setChangeList(prev => [...prev, existingRegion.id])
+                    }
+                    break;
+                }
+            }
+
+            if (shouldAdd) {
+                newScheduledBlocks.push(region);
+            }
+        }
+
+        return newScheduledBlocks;
+    }
 
     const onBackgroundClick = (index: number, percentY: number) => {
         const newHourPercent = Math.max(0, Math.floor(percentY * 48 - 0.5) / 48);
 
         setScheduledBlocks(prev => {
-            return [...prev, {
+            return checkRedundancy([...prev, {
                 id: undefined,
                 from: (index + newHourPercent) * 24,
                 to: (index + newHourPercent) * 24 + Math.min(1, (1 - newHourPercent) * 24),
-            }];
+            }]);
         });
     }
+
+    const updateZoneChanges = useCallback(((zone: ZoneInfo) => {
+        if (changeList.length === 0) return;
+        const createList: NotCreatedRegion[] = [];
+        const updateList: Region[] = [];
+        const deleteList: string[] = [];
+
+        const scheduledBlocksMap = new Map<string, Region>();
+        for (const region of scheduledBlocks) {
+            if (region.id) {
+                scheduledBlocksMap.set(region.id, region);
+            } else {
+                // this is fine as any region without an id is not created yet
+                createList.push(region as NotCreatedRegion);
+            }
+        }
+
+        for (const changedId of changeList) {
+            const existingRegion = scheduledBlocksMap.get(changedId);
+            if (existingRegion) {
+                updateList.push(existingRegion);
+            } else {
+                deleteList.push(changedId);
+            }
+        }
+
+        createRegions({zoneId: zone?.id ?? "", regions: createList});
+        updateRegions({zoneId: zone?.id ?? "", regions: updateList});
+        deleteRegions({regionIds: deleteList});
+
+        setChangeList([]);
+    }), [changeList, createRegions, deleteRegions, scheduledBlocks, updateRegions]);
+
+    useEffect(() => {
+        // update zones on dismount
+        setScheduledBlocks(zone.regions);
+
+        return () => {
+            updateZoneChanges(zone);
+        }
+    }, [zone])
 
     if (!zone) {
         return <></>;

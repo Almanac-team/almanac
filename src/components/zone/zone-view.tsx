b788b868ffa4582d928ec89d8825aa2237cb3779
Hanmin Kim <hanminkim123@gmail.com>
2023-07-22 22:33:10 -0700

    feat: update zone view logic

--- src/components/zone/zone-view.tsx
+++ src/components/zone/zone-view.tsx
@@ -1,12 +1,13 @@
-import {type ZoneInfo} from "~/components/zone/zone-column";
-import {WeekView} from "~/components/timeline/timeline-view";
-import {type ScheduledEvent} from "~/utils/types";
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import clsx from "clsx";
+import {ZoneInfo} from "~/components/zone/models";
+import {ScheduledBlock, TimelineInteractionContext} from "~/components/timeline/models";
+import {WeekView} from "~/components/timeline/timeline-view";
 
 export function ZoneView({zone, className}: { zone?: ZoneInfo, className?: string }) {
-    const [activities, setActivities] = useState<ScheduledEvent[]>([]);
+    const [scheduledBlocks, setScheduledBlocks] = useState<ScheduledBlock[]>([]);
     const firstDayMidnight = new Date(0, 0, 0, 0, 0, 0, 0);
+
 
     if (!zone) {
         return <></>;
@@ -17,6 +18,9 @@ export function ZoneView({zone, className}: { zone?: ZoneInfo, className?: strin
             <div className="w-4 h-4 rounded-full" style={{backgroundColor: zone.color}}/>
             <div className="text-xl font-bold">{zone.name}</div>
         </div>
-        <WeekView className="w-full h-full min-h-0 border-b-2" activityList={activities} firstDayMidnight={firstDayMidnight}/>
+        <TimelineInteractionContext.Provider value={{}}>
+            <WeekView className="w-full h-full min-h-0 border-b-2" blockList={scheduledBlocks}
+                      firstDayMidnight={firstDayMidnight}/>
+        </TimelineInteractionContext.Provider>
     </div>
 }

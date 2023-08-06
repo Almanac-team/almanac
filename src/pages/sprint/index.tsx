3955d7d3eb981742b9ad0f781cdcaebb8bfb7144
Hanmin Kim <hanminkim123@gmail.com>
2023-08-06 16:01:00 -0700

    feat: create simple schedule creator

--- src/pages/sprint/index.tsx
+++ src/pages/sprint/index.tsx
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import { WeekView } from '~/components/timeline/timeline-view';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Button } from '@material-tailwind/react';
 import { type ScheduledBlock } from '~/components/timeline/models';
+import {
+    convertActivitiesToAbsActivities,
+    generateEvents,
+    scheduledBlocksFromScheduledEvents,
+} from '~/server/eventGeneration';
+import { useQueryActivityDefinitions } from '~/data/activityDefinitions/query';
+import { api } from '~/utils/api';
+import {
+    type ActivitySettingWithCompletion,
+    getActivitiesFromDefinition,
+} from '~/data/activityDefinitions/virtualActivities';
+import { type ActivitySetting } from '~/components/activity/models';
 
 export function getWeekStart(date: Date) {
     const today = date;
@@ -11,26 +23,50 @@ export function getWeekStart(date: Date) {
     return today;
 }
 
-export interface GeneratedEvent {
-    id: string;
-    activityId: string;
-    name: string;
-    date: Date;
-    hours: number;
-}
-
 export default function Home() {
-    const [activityList, setActivityList] = useState<ScheduledBlock[]>([]);
+    const [scheduledBlocks, setScheduledBlocks] = useState<ScheduledBlock[]>(
+        []
+    );
     const [firstDayMidnight] = useState<Date>(getWeekStart(new Date()));
 
-    // const { data } = api.generatedEvents.getGeneratedEvents.useQuery();
+    const { data: activityDefinitions } = useQueryActivityDefinitions();
 
-    // useEffect(() => {
-    //     if (data) {
-    //         setActivityList(data);
-    //     }
-    // }, [data]);
+    const {
+        activities,
+        activitiesMap,
+    }: {
+        activities: ActivitySetting[];
+        activitiesMap: Map<string, ActivitySetting>;
+    } = useMemo(() => {
+        if (activityDefinitions) {
+            const lastDate = new Date(
+                firstDayMidnight.getTime() + 7 * 24 * 60 * 60 * 1000
+            );
 
+            const activities: ActivitySetting[] = [];
+            const activitiesMap: Map<string, ActivitySetting> = new Map();
+
+            for (const activityDefinition of activityDefinitions) {
+                const newActivities: ActivitySettingWithCompletion[] = [
+                    ...getActivitiesFromDefinition(
+                        activityDefinition,
+                        1000,
+                        lastDate
+                    ),
+                ];
+
+                newActivities.forEach((activity) => {
+                    if (!activity.completed) {
+                        activities.push(activity);
+                        activitiesMap.set(activity.id, activity);
+                    }
+                });
+            }
+
+            return { activities, activitiesMap };
+        }
+        return { activities: [], activitiesMap: new Map() };
+    }, [activityDefinitions, firstDayMidnight]);
     return (
         <>
             <Head>
@@ -43,15 +79,16 @@ export default function Home() {
                     <Button
                         color="blue"
                         onClick={() => {
-                            setActivityList([
-                                ...activityList,
-                                {
-                                    id: Math.random().toString(36).substring(7),
-                                    name: 'Test',
-                                    date: new Date('7/19/2023'),
-                                    hours: 60,
-                                },
-                            ]);
+                            const blocks = scheduledBlocksFromScheduledEvents(
+                                generateEvents(
+                                    convertActivitiesToAbsActivities(
+                                        activities
+                                    ),
+                                    firstDayMidnight
+                                ),
+                                activitiesMap
+                            );
+                            setScheduledBlocks(blocks);
                         }}
                     >
                         Generate Greedy
@@ -59,7 +96,7 @@ export default function Home() {
                 </div>
                 <WeekView
                     className="mt-2 h-full min-h-0 w-full"
-                    blockList={activityList}
+                    blockList={scheduledBlocks}
                     firstDayMidnight={firstDayMidnight}
                 />
             </main>

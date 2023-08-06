808ec2316503a2f7bdce4febb1eb0b9505260e18
Hanmin Kim <hanminkim123@gmail.com>
2023-08-06 12:57:14 -0700

    feat: clean up eventGeneration

--- src/server/eventGeneration.ts
+++ src/server/eventGeneration.ts
@@ -1,12 +1,4 @@
-import { type GeneratedEvent } from '~/pages/sprint';
-import {
-    type EventActivitySetting,
-    type TaskActivitySetting,
-} from '~/components/activity/models';
-
-import { convertTimeConfigToInt } from "~/server/api/routers/activityDefinitions";
-import { type TimeConfig } from '~/components/time_picker/date';
-import { ZoneInfo } from '~/components/zone/models';
+import { type ZoneInfo } from '~/components/zone/models';
 
 function getNextWeekStart(weekStart: Date): Date {
     const weekEnd = new Date(weekStart);
@@ -14,22 +6,20 @@ function getNextWeekStart(weekStart: Date): Date {
     return weekEnd;
 }
 
-function findHardDeadlineTasks(
-    tasks: TaskActivitySetting[],
-    weekStart: Date
-): TaskActivitySetting[] {
-    const weekEnd = getNextWeekStart(weekStart);
-    return tasks.filter((task) => task.setting.value.at < weekEnd);
-}
-
-export interface SchedulableEvents {
+export interface ScheduledEvent {
     id: string;
     activityId: string;
     start: Date;
     end: Date;
 }
 
-type ScheduledEvents = GeneratedEvent;
+export type AbsEventActivitySetting = Omit<AbsActivitySetting, 'setting'> & {
+    setting: { type: 'event'; value: AbsEventSetting };
+};
+
+export type AbsTaskActivitySetting = Omit<AbsActivitySetting, 'setting'> & {
+    setting: { type: 'task'; value: AbsTaskSetting };
+};
 
 export interface AbsActivitySetting {
     id: string;
@@ -46,190 +36,237 @@ export interface AbsActivitySetting {
 export interface AbsTaskSetting {
     at: Date;
     estimatedRequiredTime: number;
-    deadlineMod: Date;
-    startMod: Date;
+    softDeadline: Date;
+    earliestStart: Date;
 }
 
 export interface AbsEventSetting {
     at: Date;
     estimatedRequiredTime: number;
-    startMod: Date;
+    earliestStart: Date;
 }
 
-export function getDiff(start: Date, end: Date){
-    const diffInMs: number = end.getTime()-start.getTime();
-    const diffinMinutes: number = diffInMs/1000/60;
-    return diffinMinutes;
+export function getDiff(start: Date, end: Date) {
+    const diffInMs: number = end.getTime() - start.getTime();
+    return diffInMs / 1000 / 60;
 }
 
-function scheduleEvent(
-    existingEvents: ScheduledEvents[],
-    newEvents: SchedulableEvents[]
-): ScheduledEvents[] {
-    return [];
-}
-
-function splitTasks(tasks: TaskActivitySetting[]): SchedulableEvents[] {
-    const SchedulableEvents: SchedulableEvents[] = [];
-    for (const task of tasks) {
-        if (task.setting.value.estimatedRequiredTime > 1) {
-            // split task into multiple events
+function checkConflict(
+    newEvent: ScheduledEvent,
+    sceduledEvents: ScheduledEvent[]
+) {
+    for (const event of sceduledEvents) {
+        if (
+            (event.start <= newEvent.start && newEvent.start <= event.end) ||
+            (newEvent.start <= event.start && event.start <= newEvent.end)
+        ) {
+            return true;
         }
     }
-
-    return [];
+    return false;
 }
 
-// main function
-// export function generateEvents(
-//     tasks: TaskActivitySetting[],
-//     events: EventActivitySetting[],
-//     weekStart: Date
-// ): GeneratedEvent[] {
-//     const scheduledEvents: ScheduledEvents[] = [];
+function createSleepEvents(startDate: Date): ScheduledEvent[] {
+    const schedulableEventsList: ScheduledEvent[] = [];
 
-//     // schedule hard deadline tasks first
-//     const splitHardDeadlineTasks = splitTasks(
-//         findHardDeadlineTasks(tasks, weekStart)
-//     );
-//     scheduledEvents.push(
-//         ...scheduleEvent(scheduledEvents, splitHardDeadlineTasks)
-//     );
-
-//     return scheduledEvents;
-// }
-
-
-export function generateEvents(activities: AbsActivitySetting[]): SchedulableEvents[] {
-    function addActivityList (newEvent: SchedulableEvents) {
-        for (const currEvent of schedulableEventsList){
-            if ((currEvent.start <= newEvent.start && newEvent.start <= currEvent.end) 
-            || (newEvent.start <= currEvent.start && currEvent.start <= newEvent.end)) {
-                return false;
-            }
-        }
-        schedulableEventsList.push(newEvent);
-        schedulableEventsList.sort();
-    }
-
-    let schedulableEventsList: SchedulableEvents[] = [];
-
-
-    const currentDate = new Date();
-    const currentDayOfWeek = currentDate.getDay();
     for (let i = 0; i < 7; i++) {
-        const start = new Date(currentDate);
-        start.setDate(currentDate.getDate() + i);
-        start.setHours(21); 
+        const start = new Date(startDate);
+        start.setDate(startDate.getDate() + i);
+        start.setHours(21);
         start.setMinutes(0);
         start.setSeconds(0);
         start.setMilliseconds(0);
         const end = new Date(start);
-        if ((currentDayOfWeek + i) % 7 === 0) {
+
+        const isLastDay = startDate.getDay() + i === 6;
+        if (isLastDay) {
             end.setHours(23);
             end.setMinutes(59);
-        } else { 
+        } else {
             end.setDate(start.getDate() + 1);
             end.setHours(9);
             end.setMinutes(0);
         }
-        const sleep: SchedulableEvents = {
+
+        const sleep: ScheduledEvent = {
             id: 'sleep',
             activityId: 'sleep',
             start: start,
-            end: end
-        }
+            end: end,
+        };
         schedulableEventsList.push(sleep);
     }
 
-    const events: AbsActivitySetting[] = [];
-    const tasks: AbsActivitySetting[] = [];
-
-    for (const activity of activities){
-        if (activity.setting.type === "task"){
-            tasks.push(activity);
-        } else {
-            events.push(activity);
-        }
-    }
-
-    for (const Event of events){
-        const event = Event.setting.value;
-        const activity: SchedulableEvents = {
-            id: "",
-            activityId: Event.id,
-            start: event.at,
-            end: new Date(),
-        }
-        activity.end.setMinutes(event.at.getMinutes()+event.estimatedRequiredTime)
-        if (!addActivityList(activity)){
-            throw new Error("Events could not be scheduled")
-        }
-    }
-
-    const busyTasks: [number, AbsActivitySetting][] = []
-    for (const Activity of events){
-        if (Activity.setting.type === 'task'){
-            const task: AbsTaskSetting = Activity.setting.value;
-            const tot = getDiff(task.startMod, task.deadlineMod)
-            let use = 0;
-            for (const schedulableEvent of schedulableEventsList) {
-                if (task.startMod <= schedulableEvent.start && schedulableEvent.start <= task.deadlineMod) {
-                    use += Math.min(schedulableEvent.end.getTime(), task.deadlineMod.getTime()) - schedulableEvent.start.getTime();
-                }
-                else if ( task.startMod <= schedulableEvent.end && schedulableEvent.end <= task.deadlineMod) {
-                    use += schedulableEvent.end.getTime() - Math.max(schedulableEvent.start.getTime(), task.startMod.getTime());
-                }
-            }
-            for (let i = 0; i<Math.floor(task.estimatedRequiredTime/60); i++){
-                const tempActivity = Activity;
-                tempActivity.setting.value.estimatedRequiredTime = 60;
-                busyTasks.push([use/tot, tempActivity]);
-            }
-            const tempActivity = Activity;
-            tempActivity.setting.value.estimatedRequiredTime = task.estimatedRequiredTime % 60;
-            if (tempActivity.setting.value.estimatedRequiredTime){
-                busyTasks.push([use/tot, tempActivity]);
-            }
-        } else {
-            // this will never happen lmao
-        }
-    }
-    busyTasks.sort();
-    busyTasks.reverse();
-
-    for (const activityTuple of busyTasks){
-        if (activityTuple[1].setting.type === 'task'){
-            const task: AbsTaskSetting = activityTuple[1].setting.value;
-            let found = false;
-            for (let i = 0; i < schedulableEventsList.length - 1; i++){
-                const currActivity = schedulableEventsList[i];
-                const nextActivity = schedulableEventsList[i+1];
-                if (!currActivity || !nextActivity) break;
-                if (currActivity.end <= task.startMod && task.at <= nextActivity.start){
-                    const start: Date = new Date(currActivity.end.getTime());
-                    start.setMinutes(start.getMinutes() + 1);
-
-                    const end: Date = new Date(currActivity.end.getTime());
-                    end.setMinutes(end.getMinutes() + task.estimatedRequiredTime);
-                    const activity: SchedulableEvents = {
-                        id: '',
-                        activityId: activityTuple[1].id,
-                        start:start,
-                        end: end,
-                    }
-                    found = true;
-                    break;
-                } 
-            }
-            if (!found) {
-                throw Error("task could not be scheduled!")
-            }
-        } else {
-            // bruh
-        }
-    }
-    schedulableEventsList = schedulableEventsList.filter((activity) => activity.id !== 'sleep')
     return schedulableEventsList;
+}
 
+function splitActivities(activities: AbsActivitySetting[]) {
+    const events: AbsEventActivitySetting[] = [];
+    const tasks: AbsTaskActivitySetting[] = [];
+
+    for (const activity of activities) {
+        if (activity.setting.type === 'task') {
+            tasks.push(activity as AbsTaskActivitySetting);
+        } else {
+            events.push(activity as AbsEventActivitySetting);
+        }
+    }
+
+    return { events, tasks };
+}
+
+function createScheduledEventFromEvent(
+    eventActivity: AbsEventActivitySetting,
+    scheduledEvents: ScheduledEvent[]
+): ScheduledEvent | null {
+    const event = eventActivity.setting.value;
+
+    const activity: ScheduledEvent = {
+        id: '',
+        activityId: eventActivity.id,
+        start: event.at,
+        end: new Date(),
+    };
+
+    activity.end.setMinutes(
+        event.at.getMinutes() + event.estimatedRequiredTime
+    );
+
+    if (checkConflict(activity, scheduledEvents)) {
+        return null;
+    } else {
+        return activity;
+    }
+}
+
+function createPartitionedTasksWithPriorityFromTasks(
+    taskActivity: AbsTaskActivitySetting,
+    scheduledEvents: ScheduledEvent[]
+): [number, AbsTaskActivitySetting][] {
+    const partitionedTasks: [number, AbsTaskActivitySetting][] = [];
+    const taskSetting: AbsTaskSetting = taskActivity.setting.value;
+
+    const allowedTime = getDiff(
+        taskSetting.earliestStart,
+        taskSetting.softDeadline
+    );
+    let overlapTime = 0;
+    for (const scheduledEvent of scheduledEvents) {
+        if (
+            taskSetting.earliestStart <= scheduledEvent.start &&
+            scheduledEvent.start <= taskSetting.softDeadline
+        ) {
+            overlapTime +=
+                Math.min(
+                    scheduledEvent.end.getTime(),
+                    taskSetting.softDeadline.getTime()
+                ) - scheduledEvent.start.getTime();
+        } else if (
+            taskSetting.earliestStart <= scheduledEvent.end &&
+            scheduledEvent.end <= taskSetting.softDeadline
+        ) {
+            overlapTime +=
+                scheduledEvent.end.getTime() -
+                Math.max(
+                    scheduledEvent.start.getTime(),
+                    taskSetting.earliestStart.getTime()
+                );
+        }
+    }
+
+    const sectionCount = Math.floor(taskSetting.estimatedRequiredTime / 60);
+    for (let i = 0; i < sectionCount; i++) {
+        const tempActivity: AbsTaskActivitySetting = {
+            ...taskActivity,
+            setting: {
+                type: 'task',
+                value: {
+                    ...taskSetting,
+                    estimatedRequiredTime:
+                        taskSetting.estimatedRequiredTime / sectionCount,
+                },
+            },
+        };
+        partitionedTasks.push([overlapTime / allowedTime, tempActivity]);
+    }
+
+    return partitionedTasks;
+}
+
+function sortScheduledEventsByStart(scheduledEvents: ScheduledEvent[]) {
+    scheduledEvents.sort((a, b) => a.start.getTime() - b.start.getTime());
+}
+
+export function generateEvents(
+    activities: AbsActivitySetting[]
+): ScheduledEvent[] {
+    let scheduledEvents: ScheduledEvent[] = [];
+
+    const currentDate = new Date();
+
+    scheduledEvents.push(...createSleepEvents(currentDate));
+    sortScheduledEventsByStart(scheduledEvents);
+
+    const { events, tasks } = splitActivities(activities);
+
+    for (const eventActivity of events) {
+        const scheduledEvent = createScheduledEventFromEvent(
+            eventActivity,
+            scheduledEvents
+        );
+
+        if (scheduledEvent) {
+            scheduledEvents.push(scheduledEvent);
+            sortScheduledEventsByStart(scheduledEvents);
+        } else {
+            console.log('event conflict');
+        }
+    }
+
+    const busyTasks: [number, AbsTaskActivitySetting][] = [];
+    for (const taskActivity of tasks) {
+        const newBusyTasks = createPartitionedTasksWithPriorityFromTasks(
+            taskActivity,
+            scheduledEvents
+        );
+        busyTasks.push(...newBusyTasks);
+    }
+    busyTasks.sort((a, b) => b[0] - a[0]);
+
+    for (const activityTuple of busyTasks) {
+        const task: AbsTaskSetting = activityTuple[1].setting.value;
+        let found = false;
+        for (let i = 0; i < scheduledEvents.length - 1; i++) {
+            const currActivity = scheduledEvents[i];
+            const nextActivity = scheduledEvents[i + 1];
+            if (!currActivity || !nextActivity) break;
+            if (
+                currActivity.end <= task.earliestStart &&
+                task.at <= nextActivity.start
+            ) {
+                const start: Date = new Date(currActivity.end.getTime());
+                start.setMinutes(start.getMinutes() + 1);
+
+                const end: Date = new Date(currActivity.end.getTime());
+                end.setMinutes(end.getMinutes() + task.estimatedRequiredTime);
+                const activity: ScheduledEvent = {
+                    id: '',
+                    activityId: activityTuple[1].id,
+                    start: start,
+                    end: end,
+                };
+                scheduledEvents.push(activity);
+                sortScheduledEventsByStart(scheduledEvents);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            console.log('task conflict');
+        }
+    }
+    scheduledEvents = scheduledEvents.filter(
+        (activity) => activity.id !== 'sleep'
+    );
+    return scheduledEvents;
 }

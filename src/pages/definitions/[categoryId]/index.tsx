f549feccc10e6a7d84113056f8ec47c85ecd0bdf
Hanmin Kim <hanminkim123@gmail.com>
2023-08-12 15:28:49 -0700

    feat: add better Head for [categoryId]/index

--- src/pages/definitions/[categoryId]/index.tsx
+++ src/pages/definitions/[categoryId]/index.tsx
@@ -1,29 +1,50 @@
 import { useRouter } from 'next/router';
 import { ActivityColumn } from '~/components/activity/activity-column';
 import { api } from '~/utils/api';
-import { type ReactElement } from 'react';
+import React, { type ReactElement } from 'react';
 import { Layout } from '~/components/layout';
+import Head from 'next/head';
 
 export default function Home() {
     const router = useRouter();
-    const { data, isLoading } = api.categories.getCategory.useQuery({
+    const { data } = api.categories.getCategory.useQuery({
         id: String(router.query.categoryId),
     });
 
     if (data) {
         return (
             <>
+                <Head>
+                    <title>{data.categoryName} Definitions</title>
+                    <meta
+                        name="description"
+                        content={`${data.categoryName} Definitions`}
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
                 <main className="h-full max-h-screen">
                     <div className="flex h-full flex-col">
                         <div className="h-full overflow-x-auto">
-                            {isLoading ? null : (
-                                <div className="flex h-full gap-4">
-                                    <ActivityColumn categoryInfo={data} />
-                                </div>
-                            )}
+                            <div className="flex h-full gap-4">
+                                <ActivityColumn categoryInfo={data} />
+                            </div>
                         </div>
                     </div>
                 </main>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>Loading...</title>
+                    <meta
+                        name="description"
+                        content={`Loading Definitions...`}
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main className="h-full max-h-screen"></main>
             </>
         );
     }

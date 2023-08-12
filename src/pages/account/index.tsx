6f4994cf7ec3851b210fe2fe679d16f4229a415e
Hanmin Kim <hanminkim123@gmail.com>
2023-08-12 13:39:10 -0700

    feat: clean up main page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,16 +1,26 @@
 import Head from 'next/head';
-import {withAuthServerSideProps} from "~/components/generic/withAuthServerSide";
+import { withAuthServerSideProps } from '~/components/generic/withAuthServerSide';
+import { signOut } from 'next-auth/react';
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>Calendar</title>
+                <title>Account</title>
                 <meta name="description" content="Sprint" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="max-h-screen">
                 <div className="flex max-h-screen w-full overflow-y-hidden"></div>
+
+                <div>
+                    <button
+                        className="rounded-s border-2 border-black px-2 py-1 font-semibold"
+                        onClick={() => void signOut()}
+                    >
+                        Sign out
+                    </button>
+                </div>
             </main>
         </>
     );

b1e0ea07cfb58b856091508ba8a9b6323c393ccd
Hanmin Kim <hanminkim123@gmail.com>
2023-08-13 22:31:36 -0700

    Revert "feat: remove generateEvents route"
    This reverts commit 7f5078d3e0d2e344e742d99b320c52ba019cb0e0.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from '~/server/api/trpc';
 import userSettingsRouter from './routers/userSettings';
 import categoriesRouter from '~/server/api/routers/categories';
+import generatedEventsRouter from '~/server/api/routers/generateEvents';
 import zonesRouter from '~/server/api/routers/zones';
 import regionsRouter from '~/server/api/routers/regions';
 import activityDefinitionsRouter from '~/server/api/routers/activityDefinitions';
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
     settings: userSettingsRouter,
     categories: categoriesRouter,
     activityDefinitions: activityDefinitionsRouter,
+    generatedEvents: generatedEventsRouter,
     zones: zonesRouter,
     regions: regionsRouter,
 });
---
+++ src/server/api/routers/generateEvents.ts
@@ -0,0 +1,5 @@
+import { createTRPCRouter } from '~/server/api/trpc';
+
+const generatedEventsRouter = createTRPCRouter({});
+
+export default generatedEventsRouter;

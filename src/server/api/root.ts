ed32d835ecefd08572f6c52481599d956e3e2ab6
Hanmin Kim <hanminkim123@gmail.com>
2023-08-13 22:09:53 -0700

    feat: remove generateEvents route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,6 @@
 import { createTRPCRouter } from '~/server/api/trpc';
 import userSettingsRouter from './routers/userSettings';
 import categoriesRouter from '~/server/api/routers/categories';
-import generatedEventsRouter from '~/server/api/routers/generateEvents';
 import zonesRouter from '~/server/api/routers/zones';
 import regionsRouter from '~/server/api/routers/regions';
 import activityDefinitionsRouter from '~/server/api/routers/activityDefinitions';
@@ -15,7 +14,6 @@ export const appRouter = createTRPCRouter({
     settings: userSettingsRouter,
     categories: categoriesRouter,
     activityDefinitions: activityDefinitionsRouter,
-    generatedEvents: generatedEventsRouter,
     zones: zonesRouter,
     regions: regionsRouter,
 });
